44310cf1cf575e6c11a8f9555b085da68a3c257c
LateBloomer34 <himnariyal30@gmail.com>
2024-12-21 10:47:08 +0530

    second largest in array without sorting

--- array/01Striver/easy/02.cpp
+++ array/01Striver/easy/02.cpp
@@ -1,15 +1,34 @@
-// second largest element in an array 
+// second largest element in an array  without sorting
 
 
 #include<iostream>
 using namespace std;
 
-int secondLar(int arr , int size){
-    
+int secondLar(int arr[] , int n){
+
+    int largest = INT_MIN;
+    int sec = INT_MIN;
+
+for (int i =0 ; i < n ; i++){
+    if (arr[i]>largest){
+        sec= largest;
+        largest = arr[i];
+       
+  
+    }
+  else if(arr[i]>sec && arr[i]!=largest){ //else if(arr[i]>sec && arr[i]<largest)-- we can also write
+        sec = arr[i];
+       } 
+}
+     cout<<"lar"<<largest<<endl;
+    cout<<"Slar"<<sec<<endl;       
+
+return sec;
 }
 
 int main (){
-    int arr[] = {2,4,6,5,8,7};
+    int arr[] = {10,5,10};
     int n = sizeof arr / sizeof arr[0];
-    secondLar(arr , size);
+    secondLar(arr ,n);
+    // cout<<secondLar(arr ,n);
 }
