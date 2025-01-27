e48857d2386ad648b4d6888c1ef99c042d0ac095
LateBloomer34 <himnariyal30@gmail.com>
2025-01-27 16:13:46 +0530

    upperbound in array

---
+++ revision/revision 1 striver/binary search/3.cpp
@@ -0,0 +1,31 @@
+//upper bound in array
+
+#include<iostream>
+using namespace std; 
+
+int ub(int arr[] , int n , int target){
+int start = 0 ; 
+int end = n-1;
+int ans = n;
+
+while (start <=end){
+    int mid = start+(end-start)/2;
+    if (target < arr[mid]){
+        ans = mid;
+        end = mid-1;
+    }
+    else{
+        start = mid+1;
+    }
+
+}
+    return arr[ans] ;
+}
+
+int main (){
+    int arr []  = {1,2,3,4,5,6,7,7,8,9};
+     int n = sizeof arr/ sizeof arr[0];
+     int target = 7;
+     int result = ub(arr, n, target);
+     cout<<result;
+}
