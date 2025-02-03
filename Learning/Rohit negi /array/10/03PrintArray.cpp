7ea60b4c0997ea49f64ea2ceaac2b737b3ae5719
LateBloomer34 <himnariyal30@gmail.com>
2025-02-03 07:55:10 +0530

    print :wq array

---
+++ Learning/Rohit negi /array/10/03PrintArray.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void print (int arr[] , int n ){
+for (int i = 0 ; i < n ; i++){
+    for (int j = i ; j < n ; j++ ){
+        cout<<arr[j]<<" ";
+    }
+    cout<<endl;
+}
+}
+
+int main (){
+    int arr[] = {1,2,3,4,5,6,7,};
+    int n = sizeof arr / sizeof arr[0];
+    print (arr , n);
+}
+
+
+// 1 2 3 4 5
+// 2 3 4 5
+// 3 4 5
+// 4 5
+// 5
