57961ab08c788b15d370085240e94d7d02fe3a0c
LateBloomer34 <himnariyal30@gmail.com>
2025-02-03 09:35:07 +0530

    sort by 0 ,1 2

---
+++ Learning/Rohit negi /array/10/07ShortArrayInAscending.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int sort (int arr[] , int n ){
+    int start = 0 ;
+    int mid = 0 ;
+    int end = n-1;
+
+    while(mid<end){
+        if (arr[mid]==0){
+            swap(arr[start],arr[mid]);
+            start++;
+            mid++;
+        }
+        else if (arr[mid]==1){
+            swap(arr[mid] , arr[mid]);
+            mid++;
+        }
+        else if(arr[mid]==2){
+            swap(arr[mid], arr[end]);
+            end--;
+        }
+
+        }
+        for (int i = 0 ; i < n ; i++){
+            cout<<arr[i];
+        }
+    
+
+    return 0;
+}
+
+int main (){
+    int arr[] = {2,1,0,2,1,2,0};
+    int n = sizeof arr / sizeof arr[0];
+    sort(arr , n);
+}
