e15ba9e5f82daead82e18635ca4860249877a8c1
LateBloomer34 <himnariyal30@gmail.com>
2025-02-04 17:21:55 +0530

    selection sort

---
+++ Learning/Rohit negi /array/11/01SelectionSort.cpp
@@ -0,0 +1,29 @@
+///selection sort
+
+
+#include<iostream>
+using namespace std;
+
+int SS(int arr[] , int n){
+    for (int i = 0 ; i<n ;i++){
+        for (int j = i ; j <n ; j++){
+            if (arr[j]<arr[i]){
+                int temp  = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                // swap(arr[i], arr[j]);
+            }
+        }
+    }
+
+for (int i = 0 ; i < n ;i++){
+    cout<<arr[i]<<" ";
+}
+    return 0;
+}
+
+int main (){
+    int arr [] = {3,6,7,-1,9,2,0};
+    int n = sizeof arr/ sizeof arr[0];
+    SS(arr , n );
+}
