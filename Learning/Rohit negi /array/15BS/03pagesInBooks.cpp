3d4e665314d2bbf02aa7f3860bb690a1c74658ad
LateBloomer34 <himnariyal30@gmail.com>
2025-02-11 10:30:58 +0530

    not solved

---
+++ Learning/Rohit negi /array/15BS/03pagesInBooks.cpp
@@ -0,0 +1,22 @@
+// #include<iostream>
+// using namespace std ; 
+
+// int pages(int arr[] , int n , int m , int target){
+//     int start = 0 ;
+//     int end = n-1 ; 
+
+//     while (start <= end){
+        
+//     }
+
+
+//     return 0;
+// }
+
+// int main (){
+//     int arr []= {12, 34, 67 , 90};
+//     int n = sizeof arr / sizeof arr[0];
+//     int m = 2;
+//     int target = 113;
+//     pages(arr , n , m , target);
+// }
